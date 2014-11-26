bd008d629d0c20e69778102c65dda5004fc6e264
Ali <alireza.zamani@cs.rutgers.edu>
2014-11-26 14:31:11 -0800

    add the ability of manually configuring the path for each flow

--- pox/forwarding/l2_learning.py
+++ pox/forwarding/l2_learning.py
@@ -96,131 +96,169 @@ class LearningSwitch (object):
     #          str(self.transparent))
 
     #This function can check for specific ip as destination.
-  def flow_checker (self, event):
+  def flow_checker (self, event, packet, port):
     eth_packet = event.parsed
     ip_packet = eth_packet.find('ipv4')
   
     if ip_packet is None:
-      #print '_handle_PacketIn:: doesnt have ip_payload; eth_packet=%s' % eth_packet
-      return
+	    msg = of.ofp_flow_mod()
+	    msg.match = of.ofp_match.from_packet(packet, event.port)
+	    msg.idle_timeout = 10
+	    msg.hard_timeout = 30
+	    msg.actions.append(of.ofp_action_output(port = port))
+	    msg.data = event.ofp # 6a
+	    self.connection.send(msg)
+	    return
+
     srcip = (ip_packet.srcip).toStr()
     dstip = (ip_packet.dstip).toStr()
     protocol = ip_packet.protocol
-    #print '_handle_PacketIn:: rxed from sw_dpid=%s; srcip=%s, dstip=%s, protocol=%s' % 				(event.connection.dpid,srcip,dstip,protocol)
     if protocol == 1: #icmp
-	   #print 'rxed icmp_packet=%s' % ip_packet.payload
 	    print("icmp")
-
-      #print "srcip= %s, dstip= %s" % (srcip, dstip)
-    if dstip == '10.0.2.2':
+    if dstip == '10.0.2.2' and event.dpid==1:
+	    msg = of.ofp_flow_mod()
+	    msg.match = of.ofp_match.from_packet(packet, event.port)
+	    msg.idle_timeout = 10
+	    msg.hard_timeout = 30
+	    msg.actions.append(of.ofp_action_output(port = 3))
+	    msg.data = event.ofp # 6a
+	    self.connection.send(msg)
 	    print ("hello")
+	    return
+    if dstip == '10.0.2.2' and event.dpid==2:
+	    msg = of.ofp_flow_mod()
+	    msg.match = of.ofp_match.from_packet(packet, event.port)
+	    msg.idle_timeout = 10
+	    msg.hard_timeout = 30
+	    msg.actions.append(of.ofp_action_output(port = 3))
+	    msg.data = event.ofp # 6a
+	    self.connection.send(msg)
+	    print ("hello")
+	    return
+    if dstip == '10.0.2.2' and event.dpid==3:
+	    msg = of.ofp_flow_mod()
+	    msg.match = of.ofp_match.from_packet(packet, event.port)
+	    msg.idle_timeout = 10
+	    msg.hard_timeout = 30
+	    msg.actions.append(of.ofp_action_output(port = 1))
+	    msg.data = event.ofp # 6a
+	    self.connection.send(msg)
+	    print ("hello")
+	    return
+    msg = of.ofp_flow_mod()
+    msg.match = of.ofp_match.from_packet(packet, event.port)
+    msg.idle_timeout = 10
+    msg.hard_timeout = 30
+    msg.actions.append(of.ofp_action_output(port = port))
+    msg.data = event.ofp # 6a
+    self.connection.send(msg)
 
 
   def _handle_PacketIn (self, event):
-    """
-    Handle packet in messages from the switch to implement above algorithm.
-    """
+	    """
+	    Handle packet in messages from the switch to implement above algorithm.
+	    """
 
-    packet = event.parsed
-
-
-
-    def flood (message = None):
-      """ Floods the packet """
-      msg = of.ofp_packet_out()
-      if time.time() - self.connection.connect_time >= _flood_delay:
-        # Only flood if we've been connected for a little while...
-
-        if self.hold_down_expired is False:
-          # Oh yes it is!
-          self.hold_down_expired = True
-          log.info("%s: Flood hold-down expired -- flooding",
-              dpid_to_str(event.dpid))
-
-        if message is not None: log.debug(message)
-        #log.debug("%i: flood %s -> %s", event.dpid,packet.src,packet.dst)
-        # OFPP_FLOOD is optional; on some switches you may need to change
-        # this to OFPP_ALL.
-        msg.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD))
-      else:
-        pass
-        #log.info("Holding down flood for %s", dpid_to_str(event.dpid))
-      msg.data = event.ofp
-      msg.in_port = event.port
-      self.connection.send(msg)
-
-    def drop (duration = None):
-      """
-      Drops this packet and optionally installs a flow to continue
-      dropping similar ones for a while
-      """
-      if duration is not None:
-        if not isinstance(duration, tuple):
-          duration = (duration,duration)
-        msg = of.ofp_flow_mod()
-        msg.match = of.ofp_match.from_packet(packet)
-        msg.idle_timeout = duration[0]
-        msg.hard_timeout = duration[1]
-        msg.buffer_id = event.ofp.buffer_id
-        self.connection.send(msg)
-      elif event.ofp.buffer_id is not None:
-        msg = of.ofp_packet_out()
-        msg.buffer_id = event.ofp.buffer_id
-        msg.in_port = event.port
-        self.connection.send(msg)
-
-    #This function can check for specific ip as destination.
-    def flow_checker (self, event):
-      eth_packet = event.parsed
-      ip_packet = eth_packet.find('ipv4')
-  
-      if ip_packet is None:
-        #print '_handle_PacketIn:: doesnt have ip_payload; eth_packet=%s' % eth_packet
-        return
-      srcip = (ip_packet.srcip).toStr()
-      dstip = (ip_packet.dstip).toStr()
-      protocol = ip_packet.protocol
-      #print '_handle_PacketIn:: rxed from sw_dpid=%s; srcip=%s, dstip=%s, protocol=%s' % 						(event.connection.dpid,srcip,dstip,protocol)
-      if protocol == 1: #icmp
-	    #print 'rxed icmp_packet=%s' % ip_packet.payload
-	    print("icmp")
-
-      #print "srcip= %s, dstip= %s" % (srcip, dstip)
-      if dstip == '10.0.2.2':
-	    print ("hello")
+	    packet = event.parsed
 
 
-    self.macToPort[packet.src] = event.port # 1
 
-    if not self.transparent: # 2
-      if packet.type == packet.LLDP_TYPE or packet.dst.isBridgeFiltered():
-        drop() # 2a
-        return
+	    def flood (message = None):
+	      """ Floods the packet """
+	      msg = of.ofp_packet_out()
+	      if time.time() - self.connection.connect_time >= _flood_delay:
+		# Only flood if we've been connected for a little while...
 
-    if packet.dst.is_multicast:
-      flood() # 3a
-    else:
-      if packet.dst not in self.macToPort: # 4
-        flood("Port for %s unknown -- flooding" % (packet.dst,)) # 4a
-      else:
-        port = self.macToPort[packet.dst]
-        if port == event.port: # 5
-          # 5a
-          log.warning("Same port for packet from %s -> %s on %s.%s.  Drop."
-              % (packet.src, packet.dst, dpid_to_str(event.dpid), port))
-          drop(10)
-          return
-        # 6
-        log.debug("installing flow for %s.%i -> %s.%i" %
-                  (packet.src, event.port, packet.dst, port))
-        msg = of.ofp_flow_mod()
-        msg.match = of.ofp_match.from_packet(packet, event.port)
-        msg.idle_timeout = 10
-        msg.hard_timeout = 30
-        msg.actions.append(of.ofp_action_output(port = port))
-        msg.data = event.ofp # 6a
-        self.connection.send(msg)
-	self.flow_checker(event)
+		if self.hold_down_expired is False:
+		  # Oh yes it is!
+		  self.hold_down_expired = True
+		  log.info("%s: Flood hold-down expired -- flooding",
+		      dpid_to_str(event.dpid))
+
+		if message is not None: log.debug(message)
+		#log.debug("%i: flood %s -> %s", event.dpid,packet.src,packet.dst)
+		# OFPP_FLOOD is optional; on some switches you may need to change
+		# this to OFPP_ALL.
+		msg.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD))
+	      else:
+		pass
+		#log.info("Holding down flood for %s", dpid_to_str(event.dpid))
+	      msg.data = event.ofp
+	      msg.in_port = event.port
+	      self.connection.send(msg)
+
+	    def drop (duration = None):
+	      """
+	      Drops this packet and optionally installs a flow to continue
+	      dropping similar ones for a while
+	      """
+	      if duration is not None:
+		if not isinstance(duration, tuple):
+		  duration = (duration,duration)
+		msg = of.ofp_flow_mod()
+		msg.match = of.ofp_match.from_packet(packet)
+		msg.idle_timeout = duration[0]
+		msg.hard_timeout = duration[1]
+		msg.buffer_id = event.ofp.buffer_id
+		self.connection.send(msg)
+	      elif event.ofp.buffer_id is not None:
+		msg = of.ofp_packet_out()
+		msg.buffer_id = event.ofp.buffer_id
+		msg.in_port = event.port
+		self.connection.send(msg)
+
+	    #This function can check for specific ip as destination.
+	    def flow_checker (self, event):
+	      eth_packet = event.parsed
+	      ip_packet = eth_packet.find('ipv4')
+	  
+	      if ip_packet is None:
+		#print '_handle_PacketIn:: doesnt have ip_payload; eth_packet=%s' % eth_packet
+		return
+	      srcip = (ip_packet.srcip).toStr()
+	      dstip = (ip_packet.dstip).toStr()
+	      protocol = ip_packet.protocol
+	      #print '_handle_PacketIn:: rxed from sw_dpid=%s; srcip=%s, dstip=%s, protocol=%s' % 						(event.connection.dpid,srcip,dstip,protocol)
+	      if protocol == 1: #icmp
+		    #print 'rxed icmp_packet=%s' % ip_packet.payload
+		    print("icmp")
+
+	      #print "srcip= %s, dstip= %s" % (srcip, dstip)
+	      if dstip == '10.0.2.2':
+		    print ("hello")
+
+
+	    self.macToPort[packet.src] = event.port # 1
+
+	    if not self.transparent: # 2
+	      if packet.type == packet.LLDP_TYPE or packet.dst.isBridgeFiltered():
+		drop() # 2a
+		return
+
+	    if packet.dst.is_multicast:
+	      flood() # 3a
+	    else:
+	      if packet.dst not in self.macToPort: # 4
+		flood("Port for %s unknown -- flooding" % (packet.dst,)) # 4a
+	      else:
+		port = self.macToPort[packet.dst]
+		if port == event.port: # 5
+		  # 5a
+		  log.warning("Same port for packet from %s -> %s on %s.%s.  Drop."
+		      % (packet.src, packet.dst, dpid_to_str(event.dpid), port))
+		  drop(10)
+		  return
+		# 6
+		log.debug("installing flow for %s.%i -> %s.%i" %
+		          (packet.src, event.port, packet.dst, port))
+		msg = of.ofp_flow_mod()
+		msg.match = of.ofp_match.from_packet(packet, event.port)
+		msg.idle_timeout = 10
+		msg.hard_timeout = 30
+		msg.actions.append(of.ofp_action_output(port = port))
+		msg.data = event.ofp # 6a
+		#self.connection.send(msg)
+		self.flow_checker(event, packet, port)
 		
 
 
